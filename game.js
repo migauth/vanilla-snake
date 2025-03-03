846e4b630b0748777c48144d30a089adc3ab9cc5
Michael Gauthier <mikegauthier222@hotmail.com>
2025-03-02 17:28:16 -0800

    add snake square

--- game.js
+++ game.js
@@ -15,15 +15,32 @@
  * - 
  */
 
-const gameBoard = document.getElementById("game-board")
+const gameBoard = document.getElementById("game-board");
+const context = gameBoard.getContext("2d");
+
+context.fillStyle = 'purple'
+
+const SQUARE_HEIGHT = 25
+const SQUARE_WIDTH = 25
+
+const CENTER = 225;
+
 
 console.log(gameBoard);
+
+// snake
+
+const SNAKE_HEIGHT = 25;
+const SNAKE_WIDTH = 25;
+
+// first two are the x and y, second two are width and height
+context.fillRect(CENTER, CENTER, SNAKE_WIDTH, SNAKE_HEIGHT);
 
 document.addEventListener("keydown", (e) => {
   const key = e.key;
   if (key === "ArrowUp" || key === "w") {
     console.log('up');
-  }
+  }``
   if (key === "ArrowDown") {
     console.log('down');
   }
