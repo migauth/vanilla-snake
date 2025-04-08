bbbb41b4e472dae0d9288b7d9c148b271078fcbd
Michael Gauthier <mikegauthier222@hotmail.com>
2025-04-07 21:17:44 -0700

    add basic game over and score dispaly

--- game.js
+++ game.js
@@ -25,32 +25,39 @@ const unitSize = 25;
 let running = false;
 
 let score = 0;
+const scoreNum = document.getElementById("scoreNum");
+
 
 console.log(score);
 
-// console.log(gameBoard);
-
 // game board
+
 const gameBoardColour = "lightgrey";
+
 
 // snake
 
+let snakeLength = 1;
+
 const snakeColour = "purple";
 let snakePosition = { x: 25, y: 25 };
-let foodPosition = { x: 100, y: 100 }
 
 function snake() {
 
   context.fillStyle = snakeColour;
   // first two are the x and y, second two are width and height
   context.fillRect(snakePosition.x, snakePosition.y, unitSize, unitSize);
+  if (snakeLength === 2) {
+    context.fillRect(snakePosition.x - 25, snakePosition.y - 25, unitSize, unitSize);
+  }
 }
 
-// snake()
 
 // food
 
 //this works but needs to be multiples of 25 between 0 and 500
+let foodPosition = { x: 100, y: 100 }
+
 function foodPositionCalc() {
   let max = 20
   let min = 0
@@ -61,7 +68,6 @@ function foodPositionCalc() {
 }
 
 console.log('food x', foodPosition.x, 'food y', foodPosition.y);
-
 
 function drawFood(){
   context.fillStyle = "yellow";
@@ -90,12 +96,13 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-// game loop
-
+// clear game screen
 function clear() {
   context.fillStyle = gameBoardColour;
   context.fillRect(0, 0, gameBoard.width, gameBoard.height);
 }
+
+// game loop
 
 function gameLoop() {
   running = true;
@@ -106,23 +113,31 @@ function gameLoop() {
 function updateGame() {
   
   if (running) {
-    if (snakePosition.x && snakePosition.y === foodPosition.x && foodPosition.y) {
+    if (snakePosition.x === foodPosition.x && snakePosition.y === foodPosition.y) {
       console.log('yumyum');
       let positions = foodPositionCalc()
-      console.log(positions);
+      console.log('positions', positions);
       foodPosition.x = positions[0]
       foodPosition.y = positions[1]
-      console.log('food x', foodPosition.x, 'food y', foodPosition.y);
+      console.log('1food x', foodPosition.x, '1food y', foodPosition.y);
       score = score + 1
+      snakeLength = snakeLength + 1
+    }
+    if (snakePosition.x < 0 || snakePosition.x > 475) {
+      running = false
+      score = 'game over'
+    } else if (snakePosition.y < 0 || snakePosition.y > 475) {
+      running = false
+      score = 'game over'
     }
     setTimeout(() => {
       clear();
       drawFood()
       snake()
-      
+      scoreNum.innerText = score
       updateGame();
       console.log('snake x', snakePosition.x, 'food y', snakePosition.y);
-      console.log('food x', foodPosition.x, 'food y', foodPosition.y);
+      console.log('2food x', foodPosition.x, '2food y', foodPosition.y);
       console.log('score', score);
 
     }, 100);
@@ -131,7 +146,4 @@ function updateGame() {
 }
 
 gameLoop();
-
-
-console.log("test");
 
